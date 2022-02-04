effe6a3531d54077e74c3f5e3740f29298d0124f
ThiagoFPMR <thiagofpmr@gmail.com>
2022-02-04 03:05:50 -0300

    added WER plot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import difflib as dl
+from jiwer import wer
 from datetime import datetime
+import matplotlib.pyplot as plt
 from write_pdf import write_pdf
 # Defines the paths to the transcription files
 truth_path = 'transcriptions/original'
@@ -10,6 +12,8 @@ now = datetime.now()
 current_time = now.strftime('%Y-%m-%d-%H-%M-%S')
 comparsions_folder = f'output/comparsions/{current_time}'
 os.makedirs(comparsions_folder)
+# Creates a vector to store Word Error Rate values for the different comparsions being made
+wer_values = []
 # Iterates over all transcription file pairs for comparsion
 for i in range(len(os.listdir(truth_path))):
     # Stores the file paths of the files to be read in the variables below
@@ -25,4 +29,13 @@ for i in range(len(os.listdir(truth_path))):
     changelog = seq_matcher.get_opcodes()
     # Creates a pdf file comparing the two strings and the changes necessary to turn one into another
     write_pdf(new, og, changelog, f'{comparsions_folder}/{os.listdir(truth_path)[i][:-4]}.pdf')
+    # Stores the Word Error Rate into a vector
+    wer_values.append(wer(' '.join(og), ' '.join(new)))
+
+plt.style.use('fivethirtyeight')
+fig, axs = plt.subplots()
+fig.tight_layout(pad=2)
+axs.set_title("WER")
+axs.bar([c[:-4] for c in os.listdir(comparsions_folder)], wer_values)
+plt.savefig(f'{comparsions_folder}/wer.jpg')
 
